f85518c0413cb799817d30043eb32ac8312d9e14
Samuel Moor-Smith <samuelmoor-smith@Samuels-MacBook-Pro.local>
2023-07-03 18:50:21 -0700

    update XERC1155.sol to store URIs and add frontend

--- evm/tasks/index.ts
+++ evm/tasks/index.ts
@@ -1,5 +1,6 @@
 import "./deploy/PingPong";
 import "./deploy/XERC1155";
+import "./deploy/XERC1155WithURIs";
 import "./deploy/XERC721";
 import "./deploy/XERC20";
 import "./storeDeployments";
---
+++ frontend/pages/_app.js
@@ -0,0 +1,66 @@
+import "../styles/globals.css";
+import "@rainbow-me/rainbowkit/styles.css";
+
+import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { configureChains, createClient, WagmiConfig } from "wagmi";
+import {
+	mainnet,
+	polygon,
+	optimism,
+	arbitrum,
+	goerli,
+	polygonMumbai,
+	optimismGoerli,
+	arbitrumGoerli,
+} from "wagmi/chains";
+import { alchemyProvider } from "wagmi/providers/alchemy";
+import { publicProvider } from "wagmi/providers/public";
+import MainLayout from "../layout/mainLayout";
+
+const { chains, provider } = configureChains(
+	[
+		mainnet,
+		goerli,
+		polygon,
+		polygonMumbai,
+		optimism,
+		optimismGoerli,
+		arbitrum,
+		arbitrumGoerli,
+	],
+	[publicProvider({ apiKey: process.env.ALCHEMY_API_KEY }), publicProvider()]
+);
+
+const projectId = "9c2527e3b9538498845df829af4b3513";
+console.log("projectId", projectId);
+
+const { connectors } = getDefaultWallets({
+	appName: "My Alchemy DApp",
+	projectId,
+	chains,
+});
+
+const wagmiClient = createClient({
+	autoConnect: true,
+	connectors,
+	provider,
+});
+
+export { WagmiConfig, RainbowKitProvider };
+function MyApp({ Component, pageProps }) {
+	return (
+		<WagmiConfig client={wagmiClient}>
+			<RainbowKitProvider
+				modalSize="compact"
+				initialChain={process.env.NEXT_PUBLIC_DEFAULT_CHAIN}
+				chains={chains}
+			>
+				<MainLayout>
+					<Component {...pageProps} />
+				</MainLayout>
+			</RainbowKitProvider>
+		</WagmiConfig>
+	);
+}
+
+export default MyApp;
